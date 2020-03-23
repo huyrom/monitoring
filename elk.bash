6cde1a1ec005b31935b88233a194588632a93b41
huyrom <59599493+huyrom@users.noreply.github.com>
2020-03-23 10:03:17 +0700

    Creat elk.bash

---
+++ elk.bash
@@ -0,0 +1,6 @@
+### Installing the ELK
+### https://www.digitalocean.com/community/tutorials/how-to-install-elasticsearch-logstash-and-kibana-elastic-stack-on-ubuntu-16-04#step-4-%E2%80%94-installing-and-configuring-filebeat
+
+### Clearing the indexes in Elastic Search
+### http://robwillis.info/2017/11/elk-stack-installing-and-configuring-curator/
+### https://sysadminwork.com/cleaning-elasticsearch-with-curator/
