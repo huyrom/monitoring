87a82a6a7c2bbd22a5412fc97da192aa68eee11c
huyrom <59599493+huyrom@users.noreply.github.com>
2020-03-23 10:34:19 +0700

    Create ci_cd_install)guide.bash

---
+++ ci_cd_install)guide.bash
@@ -0,0 +1,9 @@
+### Integrating Giblab & Jenkins
+### https://blog.cloud365.vn/Tich-hop-Jenkins-vs-Gitlab-phan1/
+
+### Jenkins Pipeline
+### https://vnsys.wordpress.com/2018/11/15/ci-cd-voi-jenkins-pipeline-cho-php-project/
+### https://blog.vietnamlab.vn/2017/11/22/pipeline-voi-jenkins/
+
+### Integrating Coverity Scan with Gitlab CI
+### https://www.synopsys.com/blogs/software-security/integrating-coverity-scan-with-gitlab-ci/
